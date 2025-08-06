236d283d7fa0afbd65062d2940ff31fd1f878ba7
Long-github-web <damlong0712@gmail.com>
2025-08-06 15:38:47 +0700

    hotel booking

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include 'db.php'; // Kết nối database
-
 // === PHẦN KIỂM TRA QUYỀN ===
 if (!isset($_SESSION['userid']) || $_SESSION['role'] !== 2) {
     header("Location: login.php");
